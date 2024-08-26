b508822a8b16e8e8db37284d5382582f51cd9b0b
Kenneth.Ajah <kenneth.ajah@stanbicibtc.com>
2024-08-26 06:24:25 +0100

    first commited

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-unknown-property */
+/* eslint-disable react/jsx-key */
 import { useState } from "react";
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Button from "./components/Button";
@@ -12,20 +14,15 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <>
-      <div className=" pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <Header />
-        <Hero />
-        <Benefits/>
-        <Collaboration/>
-        <Services/>
-        <Pricing/>
-        <Roadmap/>
-        <Footer/>
-      </div>
-    
-      <ButtonGradient />
-    </>
+     <>
+
+​<img class="float-left ..." src="path/to/image.jpg"/>
+<p>
+Maybe we can live without libraries, people like you and me. Maybe. Sure, we're too old to change the world, but what about that kid, sitting down, opening a book, right now, in a branch at the local library and finding drawings of pee-pees and wee-wees on the Cat in the Hat and the Five Chinese Brothers? Doesn't HE deserve better? Look. If you think this is about overdue fines and missing books, you'd better think again. This is about that kid's right to read a book without getting his mind warped! Or: maybe that turns you on, Seinfeld; maybe that's how y'get your kicks. You and your good-time buddies.
+
+</p>
+
+     </>
   );
 }
 
